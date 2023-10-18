98a40bf26b1e2b005b012ee11e22df74895489cb
Maxime Lemoine <maxime.lemoine1@etu.univ-lehavre.fr>
2023-10-18 23:24:21 +0200

    test

--- src/vue/PanelImage.java
+++ src/vue/PanelImage.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.Image;
 import java.awt.Component;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Color;
 import java.awt.event.*;
 
@@ -17,13 +18,18 @@ public class PanelImage extends JPanel implements MouseListener
 		this.setBackground( new Color(0,0,0,0) );
 		this.repaint();
 		this.addMouseListener( this );
+		this.setDoubleBuffered(true);
 	}
 
 	@Override
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
-		g.drawImage( this.image, 0, 0, ( (Component)(this.getParent()) ).getWidth(), ( (Component)(this.getParent()) ).getHeight(), this);
+
+		Graphics2D g2 = (Graphics2D)g;
+		g2.drawImage( this.image, 0, 0, ( (Component)(this.getParent()) ).getWidth(), ( (Component)(this.getParent()) ).getHeight(), this);
+
+		g2.dispose();
 	}
 
 	public void mousePressed(MouseEvent e){}
